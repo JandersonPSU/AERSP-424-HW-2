01235bd793041ada9fcdc1fe091803897e3fea92
JandersonPSU <jea5462@psu.edu>
2024-11-19 22:42:28 -0500

    Part 1

--- HW2_Q_1.cpp
+++ HW2_Q_1.cpp
@@ -7,3 +7,20 @@
 
 using namespace std;
 
+
+// 1. Implement an abstract base class “Sensor” 
+
+// Define the Sensor class
+class Sensor
+{
+public:
+
+    // Virtual destructor to make sure no issue pop up with derived class objects
+    virtual ~Sensor () {}
+
+    // Pure virtual function to "gather data" from the sensor
+    virtual void gatherData() const = 0;
+
+    // Pure virtual function to "process" the "gathered data"
+    virtual void processData() const = 0;
+};
