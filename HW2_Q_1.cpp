5f4bdfbdd4a33acb580f3459a49b3b559305a2db
JandersonPSU <jea5462@psu.edu>
2024-11-19 22:35:09 -0500

    Code Upload Test Q 1

---
+++ HW2_Q_1.cpp
@@ -0,0 +1,9 @@
+// This is the code for AERSP 424 HW 2 Question 1 for Jack A, Peter B, and Sean
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using namespace std;
+
